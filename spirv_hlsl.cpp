45270f618f00a994927a4e53e5b02e7a5126337d
Robert Konrad <git2016@robdangero.us>
2016-08-14 16:21:43 +0200

    Start on HLSL using a CompilerGLSL descendant

---
+++ spirv_hlsl.cpp
@@ -0,0 +1,60 @@
+/*
+* Copyright 2016 Robert Konrad
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "spirv_hlsl.hpp"
+
+using namespace spv;
+using namespace spirv_cross;
+using namespace std;
+
+string CompilerHLSL::compile()
+{
+	// Do not deal with ES-isms like precision, older extensions and such.
+	options.es = false;
+	options.version = 450;
+	backend.float_literal_suffix = true;
+	backend.double_literal_suffix = false;
+	backend.long_long_literal_suffix = true;
+	backend.uint32_t_literal_suffix = true;
+	backend.basic_int_type = "int32_t";
+	backend.basic_uint_type = "uint32_t";
+	backend.swizzle_is_function = true;
+	backend.shared_is_implied = true;
+	backend.flexible_member_array_supported = false;
+	backend.explicit_struct_type = true;
+	backend.use_initializer_list = true;
+	
+	uint32_t pass_count = 0;
+	do
+	{
+		if (pass_count >= 3)
+			throw CompilerError("Over 3 compilation loops detected. Must be a bug!");
+
+		reset();
+
+		// Move constructor for this type is broken on GCC 4.9 ...
+		buffer = unique_ptr<ostringstream>(new ostringstream());
+
+		emit_header();
+		emit_resources();
+
+		emit_function(get<SPIRFunction>(entry_point), 0);
+
+		pass_count++;
+	} while (force_recompile);
+
+	return buffer->str();
+}
