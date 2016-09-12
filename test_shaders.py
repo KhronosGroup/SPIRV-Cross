706d3ead2950ffdce2162b5f566815f9dcc2a7a7
Hans-Kristian Arntzen <hans-kristian.arntzen@arm.com>
2016-09-12 20:11:30 +0200

    Always run spirv-val for SPIR-V files created.
    Add way to disable testing if SPIR-V is not validating properly.

--- test_shaders.py
+++ test_shaders.py
@@ -66,7 +66,7 @@ def validate_shader(shader, vulkan):
     else:
         subprocess.check_call(['glslangValidator', shader])
 
-def cross_compile(shader, vulkan, spirv, eliminate):
+def cross_compile(shader, vulkan, spirv, eliminate, invalid_spirv):
     spirv_f, spirv_path = tempfile.mkstemp()
     glsl_f, glsl_path = tempfile.mkstemp(suffix = os.path.basename(shader))
     os.close(spirv_f)
@@ -81,9 +81,8 @@ def cross_compile(shader, vulkan, spirv, eliminate):
     else:
         subprocess.check_call(['glslangValidator', '-V', '-o', spirv_path, shader])
 
-    # Workaround Issue #217 in SPIRV-Tools until the issue is resolved.
-    #if spirv:
-    #    subprocess.check_call(['spirv-val', spirv_path])
+    if not invalid_spirv:
+        subprocess.check_call(['spirv-val', spirv_path])
 
     spirv_cross_path = './spirv-cross'
     if eliminate:
@@ -161,15 +160,19 @@ def shader_is_eliminate_dead_variables(shader):
 def shader_is_spirv(shader):
     return '.asm.' in shader
 
+def shader_is_invalid_spirv(shader):
+    return '.invalid.' in shader
+
 def test_shader(stats, shader, update, keep):
     joined_path = os.path.join(shader[0], shader[1])
     vulkan = shader_is_vulkan(shader[1])
     desktop = shader_is_desktop(shader[1])
     eliminate = shader_is_eliminate_dead_variables(shader[1])
     is_spirv = shader_is_spirv(shader[1])
+    invalid_spirv = shader_is_invalid_spirv(shader[1])
 
     print('Testing shader:', joined_path)
-    spirv, glsl, vulkan_glsl = cross_compile(joined_path, vulkan, is_spirv, eliminate)
+    spirv, glsl, vulkan_glsl = cross_compile(joined_path, vulkan, is_spirv, eliminate, invalid_spirv)
 
     # Only test GLSL stats if we have a shader following GL semantics.
     if stats and (not vulkan) and (not is_spirv) and (not desktop):
