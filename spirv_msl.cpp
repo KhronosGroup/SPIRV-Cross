a9fb150faaf8b93118dd02d5940a3901d7b03514
Polona Caserman <polona.caserman@gmail.com>
2017-01-26 20:05:43 +0100

    Improved loop

--- spirv_msl.cpp
+++ spirv_msl.cpp
@@ -178,14 +178,8 @@ void CompilerMSL::extract_global_variables_from_functions()
 
 	// Local vars that are declared in the main function and accessed directy by a function
 	auto &entry_func = get<SPIRFunction>(entry_point);
-	auto iter = entry_func.local_variables.begin();
-	while (iter != entry_func.local_variables.end())
-	{
-		uint32_t gv_id = *iter;
-		auto &gbl_var = get<SPIRVariable>(gv_id);
-		global_var_ids.insert(gbl_var.self);
-		iter++;
-	}
+	for (auto &var : entry_func.local_variables)
+		global_var_ids.insert(var);
 
 	std::unordered_set<uint32_t> added_arg_ids;
 	std::unordered_set<uint32_t> processed_func_ids;
