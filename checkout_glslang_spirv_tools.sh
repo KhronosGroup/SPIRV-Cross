918b451b7e11db4e8173121969ae5735f1eed86e
Hans-Kristian Arntzen <hans-kristian.arntzen@arm.com>
2017-12-11 13:22:55 +0100

    Explicitly use Unix Makefiles when building on Windows.

--- checkout_glslang_spirv_tools.sh
+++ checkout_glslang_spirv_tools.sh
@@ -21,7 +21,7 @@ cd ../..
 echo "Building glslang."
 mkdir -p external/glslang-build
 cd external/glslang-build
-cmake ../glslang -DCMAKE_BUILD_TYPE=Release
+cmake ../glslang -DCMAKE_BUILD_TYPE=Release -G"Unix Makefiles"
 make -j$(nproc)
 cd ../..
 
@@ -51,7 +51,7 @@ cd ../..
 echo "Building SPIRV-Tools."
 mkdir -p external/spirv-tools-build
 cd external/spirv-tools-build
-cmake ../spirv-tools -DCMAKE_BUILD_TYPE=Release
+cmake ../spirv-tools -DCMAKE_BUILD_TYPE=Release -G"Unix Makefiles"
 make -j$(nproc)
 cd ../..
 
