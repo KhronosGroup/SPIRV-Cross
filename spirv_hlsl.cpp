f260c452f7c3038a0110513c5f37df31e16cdf82
msiglreith <m.siglreith@gmail.com>
2018-04-08 15:04:45 +0200

    Fix naming of root constants

--- spirv_hlsl.cpp
+++ spirv_hlsl.cpp
@@ -1874,7 +1874,7 @@ void CompilerHLSL::emit_push_constant_block(const SPIRVariable &var)
 					auto backup_name = get_member_name(type.self, i);
 					auto member_name = to_member_name(type, i);
 					set_member_name(type.self, constant_index,
-					                sanitize_underscores(join(to_name(type.self), "_", member_name)));
+					                sanitize_underscores(join(to_name(var.self), "_", member_name)));
 					emit_struct_member(type, member, i, "", layout.start);
 					set_member_name(type.self, constant_index, backup_name);
 
