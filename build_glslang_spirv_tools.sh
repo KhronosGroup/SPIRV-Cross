903b798da7a4c6cc34ca5e2a9e7cccde90cd6052
Hans-Kristian Arntzen <hans-kristian.arntzen@arm.com>
2018-05-07 23:41:53 +0200

    Fix GCC 8.1 build.

--- build_glslang_spirv_tools.sh
+++ build_glslang_spirv_tools.sh
@@ -20,7 +20,7 @@ cd ../..
 echo "Building SPIRV-Tools."
 mkdir -p external/spirv-tools-build
 cd external/spirv-tools-build
-cmake ../spirv-tools -DCMAKE_BUILD_TYPE=$PROFILE -G"Unix Makefiles"
+cmake ../spirv-tools -DCMAKE_BUILD_TYPE=$PROFILE -G"Unix Makefiles" -DSPIRV_WERROR=OFF
 make -j$NPROC
 cd ../..
 
