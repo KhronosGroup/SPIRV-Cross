6157bf3caee1294153f18a703e0bcef26d011cf3
Hans-Kristian Arntzen <hans-kristian.arntzen@arm.com>
2018-10-26 10:53:11 +0200

    Add Windows support in Travis CI.
    - Add new Windows support
    - Use CMake/CTest instead of Make + shell scripts
    - Use --parallel in CTest
    - Fix CTest on Windows
    - Cleanups in test_shaders.py
    - Force specific commit for SPIRV-Headers
    - Fix Inf/NaN odd-ball case by moving to ASM

--- checkout_glslang_spirv_tools.sh
+++ checkout_glslang_spirv_tools.sh
@@ -2,6 +2,7 @@
 
 GLSLANG_REV=91ac4290bcf2cb930b4fb0981f09c00c0b6797e1
 SPIRV_TOOLS_REV=9bfe0eb25e3dfdf4f3fd86ab6c0cda009c9bd661
+SPIRV_HEADERS_REV=d5b2e1255f706ce1f88812217e9a554f299848af
 
 if [ -d external/glslang ]; then
 	echo "Updating glslang to revision $GLSLANG_REV."
@@ -35,9 +36,13 @@ fi
 if [ -d external/spirv-headers ]; then
 	cd external/spirv-headers
 	git pull origin master
+	git checkout $SPIRV_HEADERS_REV
 	cd ../..
 else
 	git clone git://github.com/KhronosGroup/SPIRV-Headers.git external/spirv-headers
+	cd external/spirv-headers
+	git checkout $SPIRV_HEADERS_REV
+	cd ../..
 fi
 
 cd ../..
