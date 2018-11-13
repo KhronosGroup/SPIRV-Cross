cf2a890e4f2d1d603a34dedb161a54671651fd21
Chip Davis <cdavis@codeweavers.com>
2018-11-13 14:11:50 -0600

    MSL: Support extended arithmetic opcodes.

--- spirv_msl.cpp
+++ spirv_msl.cpp
@@ -2005,6 +2005,21 @@ void CompilerMSL::emit_binary_unord_op(uint32_t result_type, uint32_t result_id,
 	inherit_expression_dependencies(result_id, op1);
 }
 
+static std::string type_to_max_limit(const SPIRType &type)
+{
+	switch (type.basetype)
+	{
+	case SPIRType::UByte:
+		return "UCHAR_MAX";
+	case SPIRType::UShort:
+		return "USHRT_MAX";
+	case SPIRType::UInt:
+		return "UINT_MAX";
+	default:
+		return "";
+	}
+}
+
 // Override for MSL-specific syntax instructions
 void CompilerMSL::emit_instruction(const Instruction &instruction)
 {
@@ -2483,6 +2498,57 @@ void CompilerMSL::emit_instruction(const Instruction &instruction)
 	}
 
 		// OpOuterProduct
+
+	case OpIAddCarry:
+	case OpISubBorrow:
+	{
+		uint32_t result_type = ops[0];
+		uint32_t result_id = ops[1];
+		uint32_t op0 = ops[2];
+		uint32_t op1 = ops[3];
+		forced_temporaries.insert(result_id);
+		auto &type = get<SPIRType>(result_type);
+		statement(variable_decl(type, to_name(result_id)), ";");
+		set<SPIRExpression>(result_id, to_name(result_id), result_type, true);
+
+		auto &res_type = get<SPIRType>(type.member_types[1]);
+		if (opcode == OpIAddCarry)
+		{
+			statement(to_expression(result_id), ".", to_member_name(type, 0), " = ", to_enclosed_expression(op0), " + ",
+			          to_enclosed_expression(op1), ";");
+			statement(to_expression(result_id), ".", to_member_name(type, 1), " = select(", type_to_glsl(res_type),
+			          "(1), ", type_to_glsl(res_type), "(0), addsat(", to_expression(op0), ", ", to_expression(op1),
+			          ") == ", type_to_max_limit(res_type), ");");
+		}
+		else
+		{
+			statement(to_expression(result_id), ".", to_member_name(type, 0), " = ", to_enclosed_expression(op0), " - ",
+			          to_enclosed_expression(op1), ";");
+			statement(to_expression(result_id), ".", to_member_name(type, 1), " = select(", type_to_glsl(res_type),
+			          "(1), ", type_to_glsl(res_type), "(0), subsat(", to_expression(op0), ", ", to_expression(op1),
+			          ") == 0);");
+		}
+		break;
+	}
+
+	case OpUMulExtended:
+	case OpSMulExtended:
+	{
+		uint32_t result_type = ops[0];
+		uint32_t result_id = ops[1];
+		uint32_t op0 = ops[2];
+		uint32_t op1 = ops[3];
+		forced_temporaries.insert(result_id);
+		auto &type = get<SPIRType>(result_type);
+		statement(variable_decl(type, to_name(result_id)), ";");
+		set<SPIRExpression>(result_id, to_name(result_id), result_type, true);
+
+		statement(to_expression(result_id), ".", to_member_name(type, 0), " = ", to_enclosed_expression(op0), " * ",
+		          to_enclosed_expression(op1), ";");
+		statement(to_expression(result_id), ".", to_member_name(type, 1), " = mulhi(", to_expression(op0), ", ",
+		          to_expression(op1), ");");
+		break;
+	}
 
 	default:
 		CompilerGLSL::emit_instruction(instruction);
