50342966c0451ffd7c33fb6d4b4acd35c8aae26e
Hans-Kristian Arntzen <post@arntzen-software.no>
2019-07-08 11:48:44 +0200

    Fall back to complex loop if non-trivial continue block is found.
    There is a case where we can deduce a for/while loop, but the continue
    block is actually very painful to deal with, so handle that case as
    well. Removes an exceptional case.

--- spirv_glsl.cpp
+++ spirv_glsl.cpp
@@ -11226,7 +11226,10 @@ bool CompilerGLSL::attempt_emit_loop_header(SPIRBlock &block, SPIRBlock::Method 
 			}
 
 			default:
-				SPIRV_CROSS_THROW("For/while loop detected, but need while/for loop semantics.");
+				block.disable_block_optimization = true;
+				force_recompile();
+				begin_scope(); // We'll see an end_scope() later.
+				return false;
 			}
 
 			begin_scope();
@@ -11300,7 +11303,10 @@ bool CompilerGLSL::attempt_emit_loop_header(SPIRBlock &block, SPIRBlock::Method 
 			}
 
 			default:
-				SPIRV_CROSS_THROW("For/while loop detected, but need while/for loop semantics.");
+				block.disable_block_optimization = true;
+				force_recompile();
+				begin_scope(); // We'll see an end_scope() later.
+				return false;
 			}
 
 			begin_scope();
