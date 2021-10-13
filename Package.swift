c5db4dedbc27d98c1bc551dc40f227d733726167
Taylor Holliday <taylor@audulus.com>
2021-10-13 10:53:09 -0700

    Simplify directory structure.

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SPIRV-Cross",
-            dependencies: []),
+            path: "Sources"),
     ],
     cxxLanguageStandard: .cxx14
 )
