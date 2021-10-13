54f3e14e98792ad1a86a824f35dd0a9064f4d825
Taylor Holliday <taylor@audulus.com>
2021-10-13 14:05:37 -0700

    Revert "Simplify directory structure."
    This reverts commit c5db4dedbc27d98c1bc551dc40f227d733726167.

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SPIRV-Cross",
-            path: "Sources"),
+            dependencies: []),
     ],
     cxxLanguageStandard: .cxx14
 )
