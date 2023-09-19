dada58818d8f4db6d5160a201156035312d25c05
Chip Davis <chip@holochip.com>
2023-09-19 01:08:19 -0700

    Try again to work around MSVC brokenness.

--- spirv_msl.cpp
+++ spirv_msl.cpp
@@ -3762,6 +3762,7 @@ uint32_t CompilerMSL::add_interface_block(StorageClass storage, bool patch)
 	SmallVector<SPIRVariable *> vars;
 	bool incl_builtins = storage == StorageClassOutput || is_tessellation_shader();
 	bool has_seen_barycentric = false;
+	auto &entry_point = get<SPIRFunction>(ir.default_entry_point);
 
 	InterfaceBlockMeta meta;
 
@@ -3850,7 +3851,6 @@ uint32_t CompilerMSL::add_interface_block(StorageClass storage, bool patch)
 				// We still rely on the block being declared as a variable. Make sure that happens.
 				if (all_captured && !is_builtin)
 				{
-					auto &entry_point = get<SPIRFunction>(ir.default_entry_point);
 					entry_point.add_local_variable(var_id);
 					vars_needing_early_declaration.push_back(var_id);
 				}
