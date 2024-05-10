29c3dbe065f090f7435ded74df42bae3643f0c44
Aitor Camacho <aitor@lunarg.com>
2024-05-10 17:10:22 +0100

    Add option to enforce fragment execution with side effects in MSL
    Metal will incorrectly discard fragments with side effects under
    certain circumstances prematurely. The conditions are the following:
     - Fragment will always be discarded after side effect operation
     - Pre fragment depth fails
     - Modifies depth value for a constant value in the fragment shader.
       This constant value will also fail the depth test.
    
    However, Metal will also discard the fragment even if it has
    operations with side effects inside the fragment shader before the
    discard operation.
    
    Vulkan states the graphics pipeline to execute in the following
    order:
     - Pre fragment depth test (cannot discard here due to modifying
       depth value in fragment shader)
     - Fragment shader (where the depth is modified and fragment
       discarded)
     - Post fragment depth test
    
    Therefore, we need to enforce fragment shader execution and not
    let Metal discard the fragment before that for such cases. This
    change adds an option to provide such utility.

--- test_shaders.py
+++ test_shaders.py
@@ -376,6 +376,8 @@ def cross_compile_msl(shader, spirv, opt, iterations, paths):
         msl_args.append('--msl-force-sample-rate-shading')
     if '.discard-checks.' in shader:
         msl_args.append('--msl-check-discarded-frag-stores')
+    if '.force-frag-with-side-effects-execution.' in shader:
+        msl_args.append('--msl-force-frag-with-side-effects-execution')
     if '.lod-as-grad.' in shader:
         msl_args.append('--msl-sample-dref-lod-array-as-grad')
     if '.agx-cube-grad.' in shader:
