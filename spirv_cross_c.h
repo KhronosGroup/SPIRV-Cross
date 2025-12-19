5b9f104715e87068b379e02a316d2e3b4cadde36
Hans-Kristian Arntzen <post@arntzen-software.no>
2025-12-19 12:27:24 +0100

    c: Bump API version.

--- spirv_cross_c.h
+++ spirv_cross_c.h
@@ -40,7 +40,7 @@ extern "C" {
 /* Bumped if ABI or API breaks backwards compatibility. */
 #define SPVC_C_API_VERSION_MAJOR 0
 /* Bumped if APIs or enumerations are added in a backwards compatible way. */
-#define SPVC_C_API_VERSION_MINOR 67
+#define SPVC_C_API_VERSION_MINOR 68
 /* Bumped if internal implementation details change. */
 #define SPVC_C_API_VERSION_PATCH 0
 
