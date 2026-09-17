cd06139cbbe732faddde181882f8324baa24ec15
Hans-Kristian Arntzen <post@arntzen-software.no>
2026-09-17 11:43:55 +0200

    Never pass down pointer types to matrix conversions in the first place.

--- spirv_glsl.cpp
+++ spirv_glsl.cpp
@@ -5898,7 +5898,8 @@ string CompilerGLSL::to_expression(uint32_t id, bool register_expression_read)
 			uint32_t physical_type_id = get_extended_decoration(id, SPIRVCrossDecorationPhysicalTypeID);
 			bool is_packed = has_extended_decoration(id, SPIRVCrossDecorationPhysicalTypePacked);
 			bool relaxed = has_decoration(id, DecorationRelaxedPrecision);
-			return convert_row_major_matrix(e.expression, get<SPIRType>(e.expression_type), physical_type_id,
+			auto &value_type = get_pointee_type(get<SPIRType>(e.expression_type));
+			return convert_row_major_matrix(e.expression, value_type, physical_type_id,
 			                                is_packed, relaxed);
 		}
 		else if (flattened_structs.count(id))
@@ -16879,7 +16880,11 @@ bool CompilerGLSL::member_is_packed_physical_type(const SPIRType &type, uint32_t
 string CompilerGLSL::convert_row_major_matrix(string exp_str, const SPIRType &exp_type, uint32_t /* physical_type_id */,
                                               bool /*is_packed*/, bool relaxed)
 {
+	if (is_pointer(exp_type))
+		SPIRV_CROSS_THROW("Cannot transpose a pointer type.");
+
 	strip_enclosed_expression(exp_str);
+
 	if (!is_matrix(exp_type))
 	{
 		auto column_index = exp_str.find_last_of('[');
@@ -16899,14 +16904,13 @@ string CompilerGLSL::convert_row_major_matrix(string exp_str, const SPIRType &ex
 			column_expr = column_expr.substr(end_deferred_index) +
 			              column_expr.substr(0, end_deferred_index);
 		}
-		auto &val_type = get_pointee_type(exp_type);
-		auto transposed_expr = type_to_glsl_constructor(val_type) + "(";
+		auto transposed_expr = type_to_glsl_constructor(exp_type) + "(";
 
 		// Loading a column from a row-major matrix. Unroll the load.
-		for (uint32_t c = 0; c < val_type.vecsize; c++)
+		for (uint32_t c = 0; c < exp_type.vecsize; c++)
 		{
 			transposed_expr += join(exp_str, '[', c, ']', column_expr);
-			if (c + 1 < val_type.vecsize)
+			if (c + 1 < exp_type.vecsize)
 				transposed_expr += ", ";
 		}
 
